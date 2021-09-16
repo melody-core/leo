1d0d28e9b765284afdb9230fa0b451543c0d6a70
六弦 <liuxian.wang@tuya.com>
2021-09-16 16:44:05 +0800

    fix: +pull 更新兜底

--- libs/storeContrulor.js
+++ libs/storeContrulor.js
@@ -1,6 +1,22 @@
-
+const path = require('path');
+const { exec } = require('child_process');
 const { pull, initGit, addRemote, clone, getBranchs  } = require('./gitApis')
 
+
+const removeCacheDir = ( ) => {
+    const targetPath = path.resolve(__dirname, './../cacheStore');
+    return new Promise((resolve, reject)=>{
+        exec(`rm -rf ${targetPath}/melody-template-store`, {
+            cwd: process.cwd()
+        },(err, stidio)=>{
+            if(err){
+                reject(err)
+            }else{
+                resolve(stidio)
+            }
+        })
+    })
+}
 class StoreContrulor{
     constructor(){
         this.current = {};
@@ -9,14 +25,22 @@ class StoreContrulor{
         try {
             await pull();
         } catch (error) {
+            try {
+                await removeCacheDir();
+            } catch (__error) {}
             await initGit();
             await clone();
         }
-        const remoteList = await getBranchs();
+        let remoteList;
+        try {
+            remoteList = await getBranchs();
+        } catch (error) {
+            await removeCacheDir();
+            console.warning('🦁️ 狮子刚修复了一个小bug，请重新此命令。')
+        }
         this.current = remoteList;
         return remoteList;
     }
-
 }
 
 
